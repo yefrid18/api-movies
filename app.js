de63115658538d4cd688195b467e2f5b34c6c3ad
yefrid18 <yefrid18@gmail.com>
2022-10-05 22:07:02 -0500

    crear validaciones de llamado a API, y paginado de web con botones

--- app.js
+++ app.js
@@ -1,16 +1,53 @@
+let pagina = 1
+const btnAnterior = document.getElementById('btnAnterior')
+const btnSiguiente = document.getElementById('btnSiguiente')
+
+btnSiguiente.addEventListener('click', ()=> {
+    if(pagina < 1000){
+        pagina += 1
+        cargarPeliculas()
+    }    
+})
+
+btnAnterior.addEventListener('click', ()=> {
+    if(pagina > 1){
+        pagina -= 1
+        cargarPeliculas()
+    }    
+})
+
 const cargarPeliculas = async () => {
   try {
     const respuesta = await fetch(
-      "https://api.themoviedb.org/3/movie/550?api_key=858744fc3a30dc03e67454b1158f5b72&language=es-MX"
+        `https://api.themoviedb.org/3/movie/popular?api_key=858744fc3a30dc03e67454b1158f5b72&language=es-MX&page=${pagina}`
     );
-    console.log(respuesta);
+    //console.log(respuesta);
 
-    const datos = await respuesta.json()
-    console.log(datos);
+    if (respuesta.status === 200){
+        const datos = await respuesta.json()
+
+        let peliculas = ''
+        datos.results.forEach(pelicula => {
+            peliculas += `
+            <div class="pelicula">
+                <img class="poster" src="https://image.tmdb.org/t/p/w500/${pelicula.poster_path}">            
+            <h3 class="titulo">${pelicula.title}</h3>
+            </div>
+            `            
+        })
+        document.getElementById('contenedor').innerHTML = peliculas
+
+    }else if (respuesta.status === 401){
+        console.log("The Key is wrong, Please Check it")    
+    }else if (respuesta.status === 404){
+        console.log("The Movie not exist")
+    }else{
+        console.log("Error Fatal")
+    }   
 
   } catch (error) {
-    console.log(error);
+    console.log(error)
   }
 };
 
-cargarPeliculas();
+cargarPeliculas()
